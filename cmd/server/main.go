3c249133f777e25d26f0d7e416495c111b811388
Zrossiz <soon.pluxury@mail.ru>
2024-08-03 09:40:39 +0300

    feat: created update metric route

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,21 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
-)
 
-func defaultRoute(res http.ResponseWriter, req *http.Request) {
-	res.Header().Set("Content-Type", "application/json")
-	res.Write([]byte(`
-		{
-			"success": true,
-			"message": "hello world"
-		}
-		`))
-}
+	"github.com/Zrossiz/go-metrics/internal/http/handlers"
+)
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", defaultRoute)
+	mux.HandleFunc("/update/", handlers.UpdateMetricHandler)
 	fmt.Println("Starting server on port 8080...")
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
