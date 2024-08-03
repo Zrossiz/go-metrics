f01e38f46c6aa58736bf6045de031a996c75069f
Zrossiz <soon.pluxury@mail.ru>
2024-08-03 18:14:25 +0300

    chore: added send metrick function

---
+++ internal/http/handlers/sendMetrick.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Zrossiz/go-metrics/internal/lib/collector"
+)
+
+const serverAddress = "localhost:8080"
+
+func SendMetrics(metrics []collector.Metric) {
+	for i := 0; i <= len(metrics); i++ {
+		reqUrl := fmt.Sprintf("http://%s/update/%s/%s/%f", serverAddress, metrics[i].Type, metrics[i].Name, metrics[i].Value)
+		fmt.Printf(reqUrl)
+		_, err := http.Post(reqUrl, "text/plain", nil)
+		if err != nil {
+			fmt.Println("Request: %s failed, err: %v", reqUrl, err)
+			continue
+		}
+	}
+}
