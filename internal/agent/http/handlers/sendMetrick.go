f617b263d0e20fd48c0b58bf5bcc6f054bccf6fc
Zrossiz <soon.pluxury@mail.ru>
2024-08-04 15:13:51 +0300

    fix: name of variable

--- internal/agent/http/handlers/sendMetrick.go
+++ internal/agent/http/handlers/sendMetrick.go
@@ -12,7 +12,7 @@ const serverAddress = "localhost:8080"
 func SendMetrics(metrics []collector.Metric) {
 	for i := 0; i < len(metrics); i++ {
 		reqURL := fmt.Sprintf("http://%s/update/%s/%s/%f", serverAddress, metrics[i].Type, metrics[i].Name, metrics[i].Value)
-		resp, err := http.Post(reqUrl, "text/plain", nil)
+		resp, err := http.Post(reqURL, "text/plain", nil)
 		if err != nil {
 			fmt.Println("Request: %s failed, err: %v", reqURL, err)
 			continue
