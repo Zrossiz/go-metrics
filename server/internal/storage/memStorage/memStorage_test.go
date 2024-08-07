8eaaaf686fc7e1928efa762bc698d38811cf5d74
Zrossiz <soon.pluxury@mail.ru>
2024-08-07 22:37:10 +0300

    chore: added test for set gauge

--- server/internal/storage/memStorage/memStorage_test.go
+++ server/internal/storage/memStorage/memStorage_test.go
@@ -7,20 +7,26 @@ import (
 )
 
 func TestSetCounter(t *testing.T) {
-	Metrics = append(Metrics, storage.Metric{
-		Type:  storage.CounterType,
-		Name:  "TestCounter",
-		Value: int64(0),
-	})
-
 	SetCounter("TestCounter", 5)
 	if val, ok := Metrics[0].Value.(int64); !ok || val != 5 {
 		t.Fatalf("expected value 5, got %v", Metrics[0].Value)
 	}
 
-	// Проверяем SetCounter с вторым значением
 	SetCounter("TestCounter", 5)
 	if val, ok := Metrics[0].Value.(int64); !ok || val != 10 {
 		t.Fatalf("expected value 10, got %v", Metrics[0].Value)
+	}
+}
+
+func TestSetGauge(t *testing.T) {
+	Metrics = []storage.Metric{}
+	SetGauge("TestGauge", 0.5)
+	if Metrics[0].Value != 0.5 {
+		t.Fatalf("expected value 0.5, got %v", Metrics[0].Value)
+	}
+
+	SetGauge("TestGauge", 0.6)
+	if Metrics[0].Value != 0.6 {
+		t.Fatalf("expected value 0.6, got %v", Metrics[0].Value)
 	}
 }
