3133d5ce612b909efa077a3c94ade6e810089bc8
Zrossiz <soon.pluxury@mail.ru>
2024-08-10 20:37:21 +0300

    fix: added closer for body response

--- internal/agent/http/send/send.go
+++ internal/agent/http/send/send.go
@@ -12,13 +12,14 @@ func SendMetrics(metrics []types.Metric, addr string) []types.Metric {
 
 	for i := 0; i < len(metrics); i++ {
 		reqURL := fmt.Sprintf("http://%s/update/%s/%s/%v", addr, metrics[i].Type, metrics[i].Name, metrics[i].Value)
-		_, err := http.Post(reqURL, "text/plain", nil)
+		resp, err := http.Post(reqURL, "text/plain", nil)
 		if err != nil {
 			fmt.Println("Request:", reqURL, "failed, err:", err)
 			continue
 		}
 
 		sendedMetrics = append(sendedMetrics, metrics[i])
+		resp.Body.Close()
 	}
 	return sendedMetrics
 }
