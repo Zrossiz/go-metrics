667712f009ce19cd9f9b371edcb52073ea67aee2
Zrossiz <soon.pluxury@mail.ru>
2024-08-10 20:35:13 +0300

    chore: added send metric test

---
+++ internal/agent/http/send/send.go
@@ -0,0 +1,24 @@
+package send
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Zrossiz/go-metrics/internal/agent/constants/types"
+)
+
+func SendMetrics(metrics []types.Metric, addr string) []types.Metric {
+	var sendedMetrics []types.Metric
+
+	for i := 0; i < len(metrics); i++ {
+		reqURL := fmt.Sprintf("http://%s/update/%s/%s/%v", addr, metrics[i].Type, metrics[i].Name, metrics[i].Value)
+		_, err := http.Post(reqURL, "text/plain", nil)
+		if err != nil {
+			fmt.Println("Request:", reqURL, "failed, err:", err)
+			continue
+		}
+
+		sendedMetrics = append(sendedMetrics, metrics[i])
+	}
+	return sendedMetrics
+}
---
+++ internal/agent/http/send/send_test.go
@@ -0,0 +1,36 @@
+package send
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Zrossiz/go-metrics/internal/agent/constants"
+	"github.com/Zrossiz/go-metrics/internal/agent/constants/types"
+)
+
+func TestSendMetrics(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer srv.Close()
+
+	metrics := []types.Metric{
+		{Name: "metricCounter", Type: constants.Counter, Value: 100},
+		{Name: "metricGauge", Type: constants.Gauge, Value: 0.2},
+	}
+
+	expectedMetrics := metrics
+
+	sendedMetrics := SendMetrics(metrics, "localhost:8080")
+
+	if len(sendedMetrics) != len(expectedMetrics) {
+		t.Errorf("Expectd %d metrics to be sent, but got %d", len(expectedMetrics), len(sendedMetrics))
+	}
+
+	for i, metric := range sendedMetrics {
+		if metric != expectedMetrics[i] {
+			t.Errorf("Expected metric %v, but got %v", expectedMetrics[i], metric)
+		}
+	}
+}
