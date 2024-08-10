427a6c037d828f92ca20834b24363bcca16b28dc
Zrossiz <soon.pluxury@mail.ru>
2024-08-10 14:56:27 +0300

    fix: get flags from cli

--- internal/agent/config/config.go
+++ internal/agent/config/config.go
@@ -21,26 +21,25 @@ func FlagParse() {
 		log.Println("Файл .env загружен успешно")
 	}
 
+	flag.StringVar(&RunAddr, "a", "localhost:8080", "address and port to run server")
+	flag.Int64Var(&PollInterval, "p", 2, "interval for get metrics")
+	flag.Int64Var(&ReportInterval, "r", 10, "interval for send metrics")
+
+	flag.Parse()
+
 	if envRunAddr := os.Getenv("ADDRESS_SERVER"); envRunAddr != "" {
 		RunAddr = envRunAddr
-	} else {
-		flag.StringVar(&RunAddr, "a", "localhost:8080", "address and port to run server")
 	}
 
 	if envPollInterval := os.Getenv("POLL_INTERVAL"); envPollInterval != "" {
-
 		if val, err := strconv.ParseInt(envPollInterval, 10, 64); err == nil {
 			PollInterval = val
 		}
-	} else {
-		flag.Int64Var(&PollInterval, "p", 2, "interval for get metrics")
 	}
 
 	if envReportInterval := os.Getenv("REPORT_INTERVAL"); envReportInterval != "" {
 		if val, err := strconv.ParseInt(envReportInterval, 10, 64); err == nil {
 			ReportInterval = val
 		}
-	} else {
-		flag.Int64Var(&ReportInterval, "r", 10, "interval for send metrics")
 	}
 }
