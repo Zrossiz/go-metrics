33256e52c4bef50413b1d1a6eb2ed859dae362df
Zrossiz <soon.pluxury@mail.ru>
2024-08-21 15:48:55 +0300

    refactor: back to handle iterate)

--- internal/agent/services/collector/collector.go
+++ internal/agent/services/collector/collector.go
@@ -2,49 +2,43 @@ package collector
 
 import (
 	"math/rand"
-	"reflect"
 	"runtime"
 
-	"github.com/Zrossiz/go-metrics/internal/agent/constants"
 	"github.com/Zrossiz/go-metrics/internal/agent/constants/types"
 )
 
 func GetMetrics() []types.Metric {
-	var metrics []types.Metric
-	var memStats runtime.MemStats
-	runtime.ReadMemStats(&memStats)
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
 
-	v := reflect.ValueOf(memStats)
-	t := v.Type()
-	for i := 0; i < v.NumField(); i++ {
-		fieldName := t.Field(i).Name
-		fieldValue := v.Field(i)
-
-		var value float64
-
-		switch fieldValue.Kind() {
-		case reflect.Float32, reflect.Float64:
-			value = fieldValue.Float()
-		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			value = float64(fieldValue.Int())
-		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-			value = float64(fieldValue.Uint())
-		default:
-			continue
-		}
-
-		metrics = append(metrics, types.Metric{
-			Type:  constants.Gauge,
-			Name:  fieldName,
-			Value: value,
-		})
+	return []types.Metric{
+		{Name: "Alloc", Type: "gauge", Value: float64(m.Alloc)},
+		{Name: "BuckHashSys", Type: "gauge", Value: float64(m.BuckHashSys)},
+		{Name: "Frees", Type: "gauge", Value: float64(m.Frees)},
+		{Name: "GCCPUFraction", Type: "gauge", Value: m.GCCPUFraction},
+		{Name: "GCSys", Type: "gauge", Value: float64(m.GCSys)},
+		{Name: "HeapAlloc", Type: "gauge", Value: float64(m.HeapAlloc)},
+		{Name: "HeapIdle", Type: "gauge", Value: float64(m.HeapIdle)},
+		{Name: "HeapInuse", Type: "gauge", Value: float64(m.HeapInuse)},
+		{Name: "HeapObjects", Type: "gauge", Value: float64(m.HeapObjects)},
+		{Name: "HeapReleased", Type: "gauge", Value: float64(m.HeapReleased)},
+		{Name: "HeapSys", Type: "gauge", Value: float64(m.HeapSys)},
+		{Name: "LastGC", Type: "gauge", Value: float64(m.LastGC)},
+		{Name: "Lookups", Type: "gauge", Value: float64(m.Lookups)},
+		{Name: "MCacheInuse", Type: "gauge", Value: float64(m.MCacheInuse)},
+		{Name: "MCacheSys", Type: "gauge", Value: float64(m.MCacheSys)},
+		{Name: "MSpanInuse", Type: "gauge", Value: float64(m.MSpanInuse)},
+		{Name: "MSpanSys", Type: "gauge", Value: float64(m.MSpanSys)},
+		{Name: "Mallocs", Type: "gauge", Value: float64(m.Mallocs)},
+		{Name: "NextGC", Type: "gauge", Value: float64(m.NextGC)},
+		{Name: "NumForcedGC", Type: "gauge", Value: float64(m.NumForcedGC)},
+		{Name: "NumGC", Type: "gauge", Value: float64(m.NumGC)},
+		{Name: "OtherSys", Type: "gauge", Value: float64(m.OtherSys)},
+		{Name: "PauseTotalNs", Type: "gauge", Value: float64(m.PauseTotalNs)},
+		{Name: "StackInuse", Type: "gauge", Value: float64(m.StackInuse)},
+		{Name: "StackSys", Type: "gauge", Value: float64(m.StackSys)},
+		{Name: "Sys", Type: "gauge", Value: float64(m.Sys)},
+		{Name: "TotalAlloc", Type: "gauge", Value: float64(m.TotalAlloc)},
+		{Name: "RandomValue", Type: "gauge", Value: rand.Float64()},
 	}
-
-	metrics = append(metrics, types.Metric{
-		Type:  constants.Gauge,
-		Name:  "RandomValue",
-		Value: rand.Float64(),
-	})
-
-	return metrics
 }
