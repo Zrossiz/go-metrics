fcb6f53f910f219a0c97ab2b8028ff51e7154a28
Zrossiz <soon.pluxury@mail.ru>
2024-08-28 09:58:06 +0300

    feat: added support for gzip

--- internal/agent/http/send/send.go
+++ internal/agent/http/send/send.go
@@ -2,6 +2,7 @@ package send
 
 import (
 	"bytes"
+	"compress/gzip"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -40,6 +41,64 @@ func Metrics(metrics []types.Metric, addr string) []types.Metric {
 		resp, err := http.Post(reqURL, "application/json", bytes.NewBuffer(jsonData))
 		if err != nil {
 			log.Println("Request:", reqURL, "failed, err:", err)
+			continue
+		}
+
+		sendedMetrics = append(sendedMetrics, metrics[i])
+		resp.Body.Close()
+	}
+	return sendedMetrics
+}
+
+func GzipMetrics(metrics []types.Metric, addr string) []types.Metric {
+	var sendedMetrics []types.Metric
+
+	for i := 0; i < len(metrics); i++ {
+		reqURL := fmt.Sprintf("http://%s/update/", addr)
+
+		jsonBody := dto.MetricDTO{
+			ID:    metrics[i].Name,
+			MType: metrics[i].Type,
+		}
+
+		switch v := metrics[i].Value.(type) {
+		case int64:
+			jsonBody.Delta = &v
+		case float64:
+			jsonBody.Value = &v
+		default:
+			log.Println("unsupported metric type for metric:", metrics[i].Name)
+		}
+
+		jsonData, err := json.Marshal(jsonBody)
+		if err != nil {
+			log.Println("failed to marshall jsonBody:", err)
+			continue
+		}
+
+		var gzippedData bytes.Buffer
+		gzipWriter := gzip.NewWriter(&gzippedData)
+
+		_, err = gzipWriter.Write(jsonData)
+		if err != nil {
+			log.Println("failed to write json to gzip:", err)
+			gzipWriter.Close()
+			continue
+		}
+
+		gzipWriter.Close()
+
+		req, err := http.NewRequest("POST", reqURL, &gzippedData)
+		if err != nil {
+			log.Println("failed to create HTTP request:", err)
+		}
+		req.Header.Set("Content-Encoding", "gzip")
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Println("Request failed:", err)
 			continue
 		}
 
--- internal/server/app/main.go
+++ internal/server/app/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Zrossiz/go-metrics/internal/server/config"
 	"github.com/Zrossiz/go-metrics/internal/server/libs/logger"
+	"github.com/Zrossiz/go-metrics/internal/server/middleware/gzip"
 	"github.com/Zrossiz/go-metrics/internal/server/middleware/logger/request"
 	"github.com/Zrossiz/go-metrics/internal/server/services/get"
 	"github.com/Zrossiz/go-metrics/internal/server/services/update"
@@ -25,10 +26,18 @@ func StartServer() error {
 		return err
 	}
 
-	shugar := logger.Log
+	zLogger := logger.Log
 
 	r.Use(func(next http.Handler) http.Handler {
 		return request.WithLogs(next)
+	})
+
+	r.Use(func(next http.Handler) http.Handler {
+		return gzip.DecompressMiddleware(next)
+	})
+
+	r.Use(func(next http.Handler) http.Handler {
+		return gzip.CompressMiddleware(next)
 	})
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
@@ -55,7 +64,7 @@ func StartServer() error {
 		})
 	})
 
-	shugar.Info("Starting server",
+	zLogger.Info("Starting server",
 		zap.String("address", config.RunAddr),
 	)
 	if err := http.ListenAndServe(config.RunAddr, r); err != nil {
