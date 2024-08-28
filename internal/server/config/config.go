59b0791929f20921b2b61b2fd7d3efce2355b161
Zrossiz <soon.pluxury@mail.ru>
2024-08-28 11:00:32 +0300

    chore: added flags for file storage

--- internal/server/config/config.go
+++ internal/server/config/config.go
@@ -3,23 +3,55 @@ package config
 import (
 	"flag"
 	"os"
+	"strconv"
 
+	"github.com/Zrossiz/go-metrics/internal/server/libs/logger"
 	"github.com/joho/godotenv"
 	"go.uber.org/zap/zapcore"
 )
 
 var (
-	RunAddr      string
-	FlagLogLevel string
+	RunAddr         string
+	StoreInterval   int
+	Restore         bool
+	FileStoragePath string
+	FlagLogLevel    string
 )
 
 func FlagParse() {
+	sugar := logger.Log
 	_ = godotenv.Load()
 
 	if envRunAddr := os.Getenv("ADDRESS"); envRunAddr != "" {
 		RunAddr = envRunAddr
 	} else {
 		flag.StringVar(&RunAddr, "a", "localhost:8080", "address and port to run server")
+	}
+
+	if envStoreInterval := os.Getenv("STORE_INTERVAL"); envStoreInterval != "" {
+		value, err := strconv.Atoi(envStoreInterval)
+		if err != nil {
+			sugar.Panic("env store interval have invalid value")
+		}
+		StoreInterval = value
+	} else {
+		flag.IntVar(&StoreInterval, "-i", 30, "interval for save metrics")
+	}
+
+	if envRestore := os.Getenv("RESTORE"); envRestore != "" {
+		value, err := strconv.ParseBool(envRestore)
+		if err != nil {
+			sugar.Panic("env restore have invalid value")
+		}
+		Restore = value
+	} else {
+		flag.BoolVar(&Restore, "-r", true, "get metrics from file")
+	}
+
+	if envFileStoragePath := os.Getenv("FILE_STORAGE_PATH"); envFileStoragePath != "" {
+		FileStoragePath = envFileStoragePath
+	} else {
+		flag.StringVar(&FileStoragePath, "-f", "", "path to storage file")
 	}
 
 	flag.Parse()
