a2b54e9b4c25cb9b0476d5b5372664f60d6f3292
Zrossiz <soon.pluxury@mail.ru>
2024-08-30 12:26:18 +0300

    refactor(storage): added file storage instead of parser

--- internal/server/app/main.go
+++ internal/server/app/main.go
@@ -10,11 +10,11 @@ import (
 
 	"github.com/Zrossiz/go-metrics/internal/server/config"
 	"github.com/Zrossiz/go-metrics/internal/server/libs/logger"
-	"github.com/Zrossiz/go-metrics/internal/server/libs/parser"
 	"github.com/Zrossiz/go-metrics/internal/server/middleware/gzip"
 	"github.com/Zrossiz/go-metrics/internal/server/middleware/logger/request"
 	"github.com/Zrossiz/go-metrics/internal/server/services/get"
 	"github.com/Zrossiz/go-metrics/internal/server/services/update"
+	"github.com/Zrossiz/go-metrics/internal/server/storage/fileStorage"
 	memstorage "github.com/Zrossiz/go-metrics/internal/server/storage/memStorage"
 	"github.com/go-chi/chi/v5"
 	"go.uber.org/zap"
@@ -34,7 +34,7 @@ func StartServer() error {
 	zLogger := logger.Log
 
 	if config.Restore {
-		_, err := parser.CollectMetricsFromFile(config.FileStoragePath, store)
+		_, err := fileStorage.CollectMetricsFromFile(config.FileStoragePath, store)
 		if err != nil {
 			zLogger.Sugar().Fatalf("Error collect metrics %v", err)
 		}
@@ -49,7 +49,7 @@ func StartServer() error {
 			select {
 			case <-ticker.C:
 				zLogger.Info("save metrics...")
-				parser.UpdateMetrics(config.FileStoragePath, zLogger, store)
+				fileStorage.UpdateMetrics(config.FileStoragePath, zLogger, store)
 			case <-stop:
 				zLogger.Info("stopping task execution")
 				return
@@ -130,7 +130,7 @@ func StartServer() error {
 }
 
 func shutdownServer(store *memstorage.MemStorage) error {
-	err := parser.UpdateMetrics(config.FileStoragePath, logger.Log, store)
+	err := fileStorage.UpdateMetrics(config.FileStoragePath, logger.Log, store)
 	if err != nil {
 		return err
 	}
--- internal/server/storage/fileStorage/fileStorage_test.go
+++ internal/server/storage/fileStorage/fileStorage_test.go
@@ -1,4 +1,4 @@
-package parser
+package fileStorage
 
 import (
 	"fmt"
