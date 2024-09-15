1b6135e35538cf24b9b94838f2989552cf04a278
Zrossiz <soon.pluxury@mail.ru>
2024-09-15 13:11:06 +0300

    chore: updated tests for agent

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ toolchain go1.23.0
 require (
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/joho/godotenv v1.5.1
+	github.com/stretchr/testify v1.8.1
 	go.uber.org/zap v1.27.0
 	golang.org/x/exp v0.0.0-20240909161429-701f63a606c0
 	gorm.io/driver/postgres v1.5.9
@@ -14,14 +15,18 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/agent/http/send/send_test.go
+++ internal/agent/http/send/send_test.go
@@ -1,37 +1,122 @@
 package send
 
 import (
-	"fmt"
+	"bytes"
+	"compress/gzip"
+	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/Zrossiz/go-metrics/internal/agent/constants/types"
+	"github.com/Zrossiz/go-metrics/internal/agent/dto"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestMetrics(t *testing.T) {
-	var expectedCounterValue int64 = 42
-
-	metrics := []types.Metric{
-		{Type: "gauge", Name: "metric1", Value: 1.23},
-		{Type: "counter", Name: "metric2", Value: expectedCounterValue},
+func createTestMetrics() []types.Metric {
+	return []types.Metric{
+		{
+			Name:  "testMetric1",
+			Type:  "gauge",
+			Value: 100.0,
+		},
+		{
+			Name:  "testMetric2",
+			Type:  "counter",
+			Value: 42.0,
+		},
 	}
+}
+
+func TestMetrics(t *testing.T) {
+	metrics := createTestMetrics()
 
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "/update/", r.URL.Path)
 		w.WriteHeader(http.StatusOK)
 	}))
 	defer server.Close()
 
-	sendedMetrics := Metrics(metrics, server.Listener.Addr().String())
+	sentMetrics := Metrics(metrics, server.URL[7:])
+	assert.Equal(t, len(metrics), len(sentMetrics), "All metrics should be sent successfully")
+}
 
-	if len(sendedMetrics) != len(metrics) {
-		fmt.Print(sendedMetrics[0].Name)
-		t.Errorf("Expected %d metrics to be sent, but got %d", len(metrics), len(sendedMetrics))
-	}
+func TestGzipMetrics(t *testing.T) {
+	metrics := createTestMetrics()
 
-	for i, metric := range sendedMetrics {
-		if metric != metrics[i] {
-			t.Errorf("Expected metric %v, but got %v", metrics[i], metric)
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "/update/", r.URL.Path)
+
+		contentEncoding := r.Header.Get("Content-Encoding")
+		assert.Equal(t, "gzip", contentEncoding, "Content should be gzipped")
+
+		gz, err := gzip.NewReader(r.Body)
+		assert.NoError(t, err)
+		defer gz.Close()
+
+		body, err := io.ReadAll(gz)
+		assert.NoError(t, err)
+
+		var received dto.MetricDTO
+		err = json.Unmarshal(body, &received)
+		assert.NoError(t, err)
+
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	sentMetrics := GzipMetrics(metrics, server.URL[7:])
+	assert.Equal(t, len(metrics), len(sentMetrics), "All metrics should be sent successfully")
+}
+
+func TestBatchGzipMetrics(t *testing.T) {
+	metrics := createTestMetrics()
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "/updates/", r.URL.Path)
+
+		contentEncoding := r.Header.Get("Content-Encoding")
+		assert.Equal(t, "gzip", contentEncoding, "Content should be gzipped")
+
+		gz, err := gzip.NewReader(r.Body)
+		assert.NoError(t, err)
+		defer gz.Close()
+
+		body, err := io.ReadAll(gz)
+		assert.NoError(t, err)
+
+		var received []types.Metric
+		err = json.Unmarshal(body, &received)
+		assert.NoError(t, err)
+
+		assert.Equal(t, len(metrics), len(received), "Should receive all metrics")
+
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	BatchGzipMetrics(metrics, server.URL[7:])
+}
+
+func TestSendWithRetry(t *testing.T) {
+	attempts := 0
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		attempts++
+		if attempts < maxRetries {
+			w.WriteHeader(http.StatusInternalServerError)
+		} else {
+			w.WriteHeader(http.StatusCreated)
 		}
-	}
+	}))
+	defer server.Close()
+
+	var data bytes.Buffer
+	request, err := getRequest("POST", server.URL, data)
+	assert.NoError(t, err)
+
+	err = sendWithRetry(request)
+	assert.NoError(t, err, "Request should succeed after retries")
+	assert.Equal(t, maxRetries, attempts, "Should attempt the request exactly maxRetries times")
 }
