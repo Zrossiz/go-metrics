3b73c2401ca277ef489653924a3db7870115c8c3
Zrossiz <soon.pluxury@mail.ru>
2024-11-29 15:22:22 +0300

    feat: added build version

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,9 +1,22 @@
 // Package main is the entry point of the application. It initializes and starts the server.
 package main
 
-import "github.com/Zrossiz/go-metrics/internal/server/app"
+import (
+	"fmt"
+
+	"github.com/Zrossiz/go-metrics/internal/server/app"
+)
 
 // main is the entry point of the application.
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
 func main() {
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
 	app.StartServer()
 }
